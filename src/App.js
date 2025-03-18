f688c45dc486d641238de96d8ec69c34d65e2441
ezgikardes <ezgikardes@gmail.com>
2025-03-18 16:27:50 +0100

    implement static currency converter UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div></div>;
-}
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
