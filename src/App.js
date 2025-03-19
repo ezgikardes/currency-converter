e63208eaa10d1a93fd398300a6a90f06e82b33ce
ezgikardes <ezgikardes@gmail.com>
2025-03-19 19:02:06 +0100

    fix: ensure amount input is treated as a number

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
       <input
         type="text"
         value={amount}
-        onChange={(e) => setAmount(e.target.value)}
+        onChange={(e) => setAmount(Number(e.target.value))}
       />
       <select value={base} onChange={(e) => setBase(e.target.value)}>
         <option value="USD">USD</option>
