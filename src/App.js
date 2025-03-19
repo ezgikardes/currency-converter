b3d4c8844bd60f58cf4430a4eaf9d427eab8b523
ezgikardes <ezgikardes@gmail.com>
2025-03-19 18:59:50 +0100

    add dynamic currency conversion functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [output, setOutput] = useState(0);
+  const [base, setBase] = useState("EUR");
+  const [target, setTarget] = useState("USD");
+
+  useEffect(
+    function () {
+      fetch(
+        `https://api.frankfurter.app/latest?amount=100&from=${base}&to=${target}`
+      )
+        .then((res) => res.json())
+        .then((data) => setOutput(amount * data.rates[target]));
+    },
+    [amount, base, target]
+  );
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <select value={base} onChange={(e) => setBase(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={target} onChange={(e) => setTarget(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{output}</p>
     </div>
   );
 }
